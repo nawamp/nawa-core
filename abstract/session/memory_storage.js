37e47a3ced760aced859709418d9a5046dedd16e
NeoAtlantis <contact@chaobai.li>
2022-10-01 20:42:21 +0200

    use map instead of object

--- abstract/session/memory_storage.js
+++ abstract/session/memory_storage.js
@@ -1,31 +1,29 @@
 import SessionData from "./SessionData";
 import { global_scope_id } from "src/identifiers/IDs";
-const _ = require("lodash");
 
 
-const session_data = {};
+const session_data = new Map();
 
 
 async function create(){
     const new_session_id = await global_scope_id();
-    session_data[new_session_id] = new SessionData({
+    session_data.set(new_session_id, new SessionData({
         session_id: new_session_id,
-    });
+    }));
     return new_session_id;
 }
 
 
 function get(session_id){
-    return session_data[session_id] || null;
+    return session_data.get(session_id) || null;
 }
 
 function remove(session_id){
-    if(session_data[session_id] === undefined) return;
-    delete session_data[session_id];
+    return session_data.delete(session_id);
 }
 
 function exists(session_id){
-    return !_.isNil(_.get(session_data, session_id, null));
+    return session_data.has(session_id);
 }
 
 export default {
